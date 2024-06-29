5baa184bccc9056a83169cf5930093e9fd600eea
Farzin ahammed <farzinahammed.in@gmail.com>
2024-06-29 21:29:42 +0530

    here updating the portion one fully responsive design

--- src/components/landingPage/portionOne.jsx
+++ src/components/landingPage/portionOne.jsx
@@ -23,41 +23,43 @@ const generalItems = [
 const PortionOne = () => {
     return (
         <>
-            <div className="flex flex-col bg-black w-[15%] h-screen gap-10 py-4">
-                <div>
+            <div className="flex flex-col bg-black w-[15%] h-screen gap-10 py-4 ">
+                <div className="">
                     <div className="flex items-center justify-center gap-3">
-                        <IoMusicalNotes color="red" size={40} />
-                        <p className="text-white font-bold text-xl">
+                        <p className="max-lg:hidden"><IoMusicalNotes color="red" size={40} /></p>
+                        <p className="lg:hidden"><IoMusicalNotes color="red" size={25} /></p>
+                        <p className="text-white font-bold text-xl max-sm:hidden max-lg:text-[10px] max-xl:text-[15px]  ">
                             <span className="text-[#e23232]">Dream</span>Music
                         </p>
                     </div>
                 </div>
-                <div className="flex-grow px-7">
+                <div className="flex-grow px-7  max-lg:px-2 ">
                     <div>
-                        <p className=" text-xs font-medium text-[#b4b4b4] py-1">MENU</p>
+                        <p className=" text-xs max-sm:text-[10px] font-medium text-[#b4b4b4] py-1">MENU</p>
                     </div>
                     <div>
                         <ul>
                             {menuItems.map((item, index) => (
                                 <div key={index}>
-                                    <li className="flex items-center  gap-3 text-[#dad8d8] py-[5.9px] font-medium text-sm cursor-pointer" >
+                                    <li className="flex items-center  gap-3 text-[#dad8d8] py-[6.7px] font-medium text-sm cursor-pointer max-sm:justify-center" >
                                         <item.icon color="red" size={22} />
-                                        {item.label}
+                                        <p className="max-sm:hidden max-md:text-[12px]">{item.label}</p>
+                                        
                                     </li>
                                 </div>
                             ))}
                         </ul>
                     </div>
                 </div>
-                <div className="mt-auto px-7">
+                <div className="mt-auto px-7  max-lg:px-2 pb-10">
                     <div>
-                        <p className=" text-xs font-medium text-[#b4b4b4] py-1">GENERAL</p>
+                        <p className=" text-xs max-sm:text-[10px] font-medium text-[#b4b4b4] py-1">GENERAL</p>
                         <div>
                             <ul>
                                 {generalItems.map((item, index) => (
-                                    <li key={index} className="flex items-center  gap-3 text-[#dad8d8] py-[5.9px] font-medium text-sm cursor-pointer">
-                                        <item.icon color="red" />
-                                        {item.label}
+                                    <li key={index} className="flex items-center  gap-3 text-[#dad8d8] py-[7px] font-medium text-sm cursor-pointer max-sm:justify-center" >
+                                        <item.icon color="red" size={22}/>
+                                        <p className="max-sm:hidden max-md:text-[12px] ">{item.label}</p>
                                     </li>
                                 ))}
                             </ul>
